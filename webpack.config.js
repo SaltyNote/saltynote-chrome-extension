56252759c1449cc506a7108d7e4b898003501de7
Hao Zhou <hzhou.me@gmail.com>
2020-12-22 14:16:19 -0800

    remove useless command and bump to 0.2.3

---
+++ webpack.config.js
@@ -0,0 +1,151 @@
+const webpack = require('webpack');
+const ejs = require('ejs');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const ExtensionReloader = require('webpack-extension-reloader');
+const { VueLoaderPlugin } = require('vue-loader');
+const TerserPlugin = require('terser-webpack-plugin');
+const { version } = require('./package.json');
+
+let proEnv;
+try {
+  proEnv = require('./env.json');
+} catch (error) {
+  console.error('========================================================================================');
+  console.error('Cannot find env.json, please make a copy of "env.example.json", and name it "env.json"');
+  console.error('========================================================================================');
+  throw new Error('Cannot find env.json, please make a copy of "env.example.json", and name it "env.json"');
+}
+
+const config = {
+  mode: process.env.NODE_ENV,
+  context: `${__dirname}/src`,
+  entry: {
+    background: './background.js',
+    'options/options': './options/options.js',
+    'overlay/overlay': './overlay/overlay.js',
+  },
+  output: {
+    path: `${__dirname}/dist`,
+    filename: '[name].js',
+  },
+  resolve: {
+    extensions: ['.js', '.vue'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.sass$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader?indentedSyntax'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|ico)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: '/images/',
+          emitFile: false,
+        },
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: '/fonts/',
+          emitFile: false,
+        },
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      global: 'window',
+    }),
+    new VueLoaderPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new CopyPlugin([
+      { from: 'icons', to: 'icons' },
+      { from: 'options/options.html', to: 'options/options.html', transform: transformHtml },
+      {
+        from: 'manifest.json',
+        to: 'manifest.json',
+        transform: content => {
+          const jsonContent = JSON.parse(content);
+          jsonContent.version = version;
+
+          if (config.mode === 'development') {
+            jsonContent.content_security_policy = "script-src 'self' 'unsafe-eval'; object-src 'self'";
+          } else if (config.mode === 'production') {
+            jsonContent.content_security_policy = "script-src 'self' https://*.saltynote.com; object-src 'self'";
+          }
+          if (process.env.RELEASE !== 'true' && proEnv.manifest_key) {
+            jsonContent.key = proEnv.manifest_key;
+          }
+          return JSON.stringify(jsonContent, null, 2);
+        },
+      },
+    ]),
+  ],
+  performance: {
+    maxEntrypointSize: 10240000,
+    maxAssetSize: 10240000,
+  },
+  // fix js code isn't UTF-8 encoded.
+  optimization: {
+    // Uncomment below line to no minimize our code.
+    // minimize: false,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          output: { ascii_only: true },
+        },
+      }),
+    ],
+  },
+};
+
+if (config.mode === 'production') {
+  config.plugins = (config.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"',
+      },
+    }),
+  ]);
+}
+
+if (process.env.HMR === 'true') {
+  config.plugins = (config.plugins || []).concat([
+    new ExtensionReloader({
+      manifest: `${__dirname}/src/manifest.json`,
+    }),
+  ]);
+}
+
+function transformHtml(content) {
+  return ejs.render(content.toString(), {
+    ...process.env,
+  });
+}
+
+module.exports = config;
